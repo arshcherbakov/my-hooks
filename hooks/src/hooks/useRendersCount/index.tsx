11957cf72ad244df3fd202c74ff10912c0f842b0
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-07-17 15:38:39 +0300

    fix code

--- hooks/src/hooks/useRendersCount/index.tsx
+++ hooks/src/hooks/useRendersCount/index.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 const useRendersCount = (): number => {
   const countRenders = useRef<number>(0);
 
-  return (countRenders.current += 1);
+  return ++countRenders.current;
 };
 
 export default useRendersCount;
